0d319bd9eacf369fabdd3d213a61b4cd42266478
Jack <71041042+jackakay@users.noreply.github.com>
2024-02-12 17:07:16 +0000

    Created the setup of the project

---
+++ rps-sim/rps-sim/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace rps_sim
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            numericUpDown3 = new NumericUpDown();
+            button1 = new Button();
+            panel1 = new Panel();
+            PaperBox = new Panel();
+            RockBox = new Panel();
+            ScissorBox = new Panel();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(668, 57);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(668, 110);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(120, 23);
+            numericUpDown2.TabIndex = 1;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(668, 163);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(120, 23);
+            numericUpDown3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(668, 219);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 33);
+            button1.TabIndex = 3;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(ScissorBox);
+            panel1.Controls.Add(RockBox);
+            panel1.Controls.Add(PaperBox);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(634, 426);
+            panel1.TabIndex = 4;
+            // 
+            // PaperBox
+            // 
+            PaperBox.Location = new Point(134, 14);
+            PaperBox.Name = "PaperBox";
+            PaperBox.Size = new Size(358, 126);
+            PaperBox.TabIndex = 0;
+            // 
+            // RockBox
+            // 
+            RockBox.Location = new Point(365, 180);
+            RockBox.Name = "RockBox";
+            RockBox.Size = new Size(229, 202);
+            RockBox.TabIndex = 1;
+            // 
+            // ScissorBox
+            // 
+            ScissorBox.Location = new Point(28, 180);
+            ScissorBox.Name = "ScissorBox";
+            ScissorBox.Size = new Size(230, 202);
+            ScissorBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Controls.Add(numericUpDown3);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown3;
+        private Button button1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Panel ScissorBox;
+        private Panel RockBox;
+        private Panel PaperBox;
+    }
+}
---
+++ rps-sim/rps-sim/Form1.cs
@@ -0,0 +1,82 @@
+namespace rps_sim
+{
+    public partial class Form1 : Form
+
+    {
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numS = (int)numericUpDown1.Value;
+            int numR = (int)numericUpDown2.Value;
+            int numP = (int)numericUpDown3.Value;
+            List<player> scissors = new List<player>();
+            List<player> rock = new List<player>();
+            List<player> paper = new List<player>();
+            
+        }
+
+        private void Spawn(Option type, int num, List<player> player_)
+        {
+            if(type== Option.Rock)
+            {
+                for(int i = 0; i < num; i++)
+                {
+                    player_.Add(
+                        new player { picture = new PictureBox { BackgroundImage = Image.FromFile("../rock.png"), BackgroundImageLayout = ImageLayout.Stretch, Size = new Size(21, 24), Location = getSpawnLoc(type) } }
+                        );
+                }
+            }
+        }
+        private Point getSpawnLoc(Option type)
+        {
+            Point loc;
+            Random rand = new Random();
+            if(type== Option.Rock)
+            {
+                int x = rand.Next(RockBox.Location.X, RockBox.Location.X+RockBox.Width);
+                int y = rand.Next(RockBox.Location.Y, RockBox.Location.Y+RockBox.Height);
+                loc = new Point(x, y);
+                return loc;
+            }else if (type == Option.Paper)
+            {
+                int x = rand.Next(PaperBox.Location.X, PaperBox.Location.X + PaperBox.Width);
+                int y = rand.Next(PaperBox.Location.Y, PaperBox.Location.Y + PaperBox.Height);
+                loc = new Point(x, y);
+                return loc;
+            } else if (type == Option.Scissors)
+            {
+                int x = rand.Next(ScissorBox.Location.X, ScissorBox.Location.X + ScissorBox.Width);
+                int y = rand.Next(ScissorBox.Location.Y, ScissorBox.Location.Y + ScissorBox.Height);
+                loc = new Point(x, y);
+                return loc;
+            }
+            return new Point(0, 0);
+            
+        }
+        enum Option
+        {
+            None,
+            Rock,
+            Paper,
+            Scissors
+        }
+    }
+
+    public class player
+    {
+        public PictureBox picture { get; set; }
+        public  int type { get; set; }
+        
+    }
+}
